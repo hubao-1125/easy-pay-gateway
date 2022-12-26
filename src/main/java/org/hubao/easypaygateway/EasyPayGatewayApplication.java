10471147d371e7629dd12522c9cf6d30c76c53d7
douhu <douhuhu1125@gmail.com>
2022-12-26 14:13:51 +0800

    Initial commit

---
+++ src/main/java/org/hubao/easypaygateway/EasyPayGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.hubao.easypaygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPayGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyPayGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hubao/easypaygateway/EasyPayGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hubao.easypaygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPayGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
