09fabe9976c545a0a46d5e9d2561242ae69cf4b7
douhu <douhuhu1125@gmail.com>
2023-01-23 17:43:42 +0800

    新增epg-core模块

---
+++ epg-core/src/main/java/org/easypay/epgcore/EpgCoreApplication.java
@@ -0,0 +1,13 @@
+package org.easypay.epgcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpgCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpgCoreApplication.class, args);
+    }
+
+}
---
+++ epg-core/src/test/java/org/easypay/epgcore/EpgCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.easypay.epgcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpgCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
